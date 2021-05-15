d14b39323e3bb537418575a60823497277f608f7
Joan <remnyachizot2015@gmail.com>
2021-05-15 17:14:55 -0500

    Adding Promise.all for Stripe API

--- ajax_examples/js/stripe-checkout.js
+++ ajax_examples/js/stripe-checkout.js
@@ -1,34 +1,49 @@
 import STRIPE_KEYS from './stripe-keys.js';
 
-// console.log(STRIPE_KEYS);
-
 const d = document;
-const $tacos = d.getElementById('ceviches');
+const $ceviches = d.getElementById('ceviches');
 const $template = d.getElementById('ceviche-template').content;
 const $fragment = d.createDocumentFragment();
 
-fetch('https://api.stripe.com/v1/products', {
+const headerOptions = {
   headers: {
-    Authorization: `Bearer ${STRIPE_KEYS.secret}`
+    Authorization: `Bearer ${ STRIPE_KEYS.secret }`
   }
-})
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
-  });
+};
 
-fetch('https://api.stripe.com/v1/prices', {
-  headers: {
-    Authorization: `Bearer ${STRIPE_KEYS.secret}`
-  }
-})
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
+let prices, products;
+
+Promise.all(
+  [
+    fetch('https://api.stripe.com/v1/products', headerOptions),
+    fetch('https://api.stripe.com/v1/prices', headerOptions)
+  ]
+).then((responses) =>
+  Promise.all(responses.map((res) => res.json()))
+).then((json) => {
+  // console.log(json);
+  products = json[0].data;
+  prices = json[1].data;
+
+  prices.forEach((el) => {
+    let productData = products.filter((product) => product.id === el.product);
+    // console.log(productData);
+
+    $template.querySelector('.ceviche').setAttribute('data-price', el.id);
+    $template.querySelector('img').src = productData[0].images[0];
+    $template.querySelector('img').alt = productData[0].name;
+    $template.querySelector('figcaption').innerHTML = `
+      ${ productData[0].name }
+      <br />
+      ${ el.unit_amount_decimal } ${ el.currency.toUpperCase() }
+    `;
+
+    let clone = d.importNode($template, true);
+    $fragment.appendChild(clone);
   });
+  $ceviches.appendChild($fragment);
+}).catch((err) => {
+  console.error(err);
+  let message = err.statusText || 'Ocurrió un error al conectarse con la API de Stripe';
+  $ceviches.innerHTML = `<p>Error ${ err.status }: ${ message }`;
+});
